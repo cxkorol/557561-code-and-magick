06ca5ba99f7b735acab4320141bf0eedd811eebc
korol <cx.korol@gmail.com>
2018-12-07 10:15:49 +0300

    Исправлен инпут файербола

--- js/setup.js
+++ js/setup.js
@@ -151,7 +151,7 @@
   var wizardCoat = setupWizard.querySelector('.wizard-coat'); // Элемент плаща
   var wizardEyes = setupWizard.querySelector('.wizard-eyes'); // Элемент глаз
   var wizardFireball = document.querySelector('.setup-fireball-wrap'); // Элемент файербола
-  var wizardFireballInput = document.querySelector('input'); // Инпут фаербола
+  var wizardFireballInput = document.querySelector('[name=fireball-color]'); // Инпут фаербола
   var coatInput = userDialog.querySelector('[name=coat-color]'); // Инпут плаща волшебника
   var eyesInput = userDialog.querySelector('[name=eyes-color]'); // Инпут глаз волшебника
 
