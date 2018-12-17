09fbe97971f6130b9002f58aab3de8257580a750
korol <cx.korol@gmail.com>
2018-12-17 13:28:29 +0300

    DRY для функции загрузки и отправки

--- js/backend.js
+++ js/backend.js
@@ -4,66 +4,47 @@
 
   var DOWNLOAD_URL = 'https://js.dump.academy/code-and-magick/data';
   var UPLOAD_URL = 'https://js.dump.academy/code-and-magick';
-  var XHR_TIMEOUT = 10000;
   var XHR_STATUS_OK = 200;
+  var XHR_TIMEOUT = 10000;
 
-  var xhrErrorHandler = function (onError) {
-    return function () {
+  var performRequest = function (url, method, onLoad, onError, data) {
+
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === XHR_STATUS_OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
       onError('Произошла ошибка соединения');
-    };
-  };
+    });
 
-  var xhrTimeoutHandler = function (xhr, onError) {
-    return function () {
+    xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    };
+    });
+
+    xhr.timeout = XHR_TIMEOUT;
+    xhr.open(method, url);
+
+    if (method === 'POST') {
+      xhr.send(data);
+    } else {
+      xhr.send();
+    }
   };
 
   var load = function (onLoad, onError) {
-
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.timeout = XHR_TIMEOUT;
-
-    var xhrLoadHandler = function () {
-      if (xhr.status === XHR_STATUS_OK) {
-        onLoad(xhr.response);
-      } else {
-        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText + '\n Не удалось загрузить данные');
-      }
-    };
-
-    xhr.addEventListener('load', xhrLoadHandler);
-    xhr.addEventListener('error', xhrErrorHandler(onError));
-    xhr.addEventListener('timeout', xhrTimeoutHandler(xhr, onError));
-
-    xhr.open('GET', DOWNLOAD_URL);
-    xhr.send();
+    performRequest(DOWNLOAD_URL, 'GET', onLoad, onError);
   };
 
   var save = function (data, onLoad, onError) {
-
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.timeout = XHR_TIMEOUT;
-
-    var xhrLoadHandler = function () {
-      if (xhr.status === XHR_STATUS_OK) {
-        onLoad(xhr.response);
-      } else {
-        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText + '\n Не удалось отправить данные');
-      }
-    };
-
-    xhr.addEventListener('load', xhrLoadHandler);
-    xhr.addEventListener('error', xhrErrorHandler(onError));
-    xhr.addEventListener('timeout', xhrTimeoutHandler(xhr, onError));
-
-    xhr.open('POST', UPLOAD_URL);
-    xhr.send(data);
-
+    performRequest(UPLOAD_URL, 'POST', onLoad, onError, data);
   };
-
 
   window.backend = {
     load: load,
